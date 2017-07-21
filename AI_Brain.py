cd05defca710de42df60f6276766335501d1789a
lawrencereyes <lawrencereyes96@gmail.com>
2017-07-21 19:03:28 -0400

    Project notes

---
+++ AI_Brain.py
@@ -0,0 +1,36 @@
+#BASIC LAYOUT OF THE PROJECT {
+#   Genetically Adversary Network used to replicate both side of the brain.
+#   Convolutional Neural Network (CNN) for classification within each aspect of the part of the brain that we are working on.
+#   LSTM to replicate how the memory process works in the brain. These would go within the CNN as the final hidden layer.
+#}
+
+#PARTS OF THE BRAIN WE ARE WORKING ON {
+#   Left Side:
+#       -Memory
+#       -Language (Hearing, Reading, Speaking)
+#       -Logic (Exact mathematical computations)
+#       -Recall of memory
+#   
+#   Right Side:
+#       -Spatial ability
+#       -Face pecognition
+#       -Musical processing
+#       -Logic (Rough estimations and comparisons)
+#       -Visual processing
+#       -Language (Interpretation of context and tone)
+#}
+
+#Another neural network will be in charge for the assignation of control and functionality to each side, ensuring the brain can maximize efficiency.
+
+#QUESTIONS THAT NEED ANSWER {
+#   Would the transmition of information through the brain (or neural networks) be linear or not linear? Feed-foward or Retrograded?
+#   Would having a LSTM as a hidden layer within a CNN be more efficient? How would that work? What would be the input given to the LSTM?
+#   What would be the ouput of the CNN?
+#   Would the cluster of NNs be more powerful than i think it would be?
+#   How about give the ability to access the internet and let it search at will? How would that work?
+#   How would we give it the concept of challenging itself to become better at will?
+#   Would that be conciousness?
+#   Can it resolve real-life problems?
+#   Do we need a better Data Compression System? So it would become smarter even faster.
+#   Maybe nature has the answer... Black Holes are in essence a Data Compression System. COULD DEFINETLY BE WRONG. 
+#}
